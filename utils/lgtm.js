0cbeca21de7fa258d6a318763659c429682fa569
MTGVim <takun617@gmail.com>
2026-03-30 23:23:28 +0900

    fix: isolate shared lgtm worker globals

--- utils/lgtm.js
+++ utils/lgtm.js
@@ -1,37 +1,37 @@
-const lgtmRoot = typeof globalThis !== 'undefined' ? globalThis : this;
+(function initGitHubMentionsLgtm(root) {
+  const curatedLgtmGifs = [
+    'https://media.giphy.com/media/l3q2XhfQ8oCkm1Ts4/giphy.gif',
+    'https://media.giphy.com/media/111ebonMs90YLu/giphy.gif',
+    'https://media.giphy.com/media/26ufdipQqU2lhNA4g/giphy.gif',
+    'https://media.giphy.com/media/3orieKZ9ax8nsJnSs8/giphy.gif',
+    'https://media.giphy.com/media/5VKbvrjxpVJCM/giphy.gif',
+    'https://media.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif',
+    'https://media.giphy.com/media/QMHoU66sBXqqLqYvGO/giphy.gif'
+  ];
 
-const CURATED_LGTM_GIFS = [
-  'https://media.giphy.com/media/l3q2XhfQ8oCkm1Ts4/giphy.gif',
-  'https://media.giphy.com/media/111ebonMs90YLu/giphy.gif',
-  'https://media.giphy.com/media/26ufdipQqU2lhNA4g/giphy.gif',
-  'https://media.giphy.com/media/3orieKZ9ax8nsJnSs8/giphy.gif',
-  'https://media.giphy.com/media/5VKbvrjxpVJCM/giphy.gif',
-  'https://media.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif',
-  'https://media.giphy.com/media/QMHoU66sBXqqLqYvGO/giphy.gif'
-];
-
-function getCuratedLgtmPool() {
-  return CURATED_LGTM_GIFS.slice();
-}
-
-function pickRandomLgtmGif(excludedUrl = null, randomFn = Math.random) {
-  const pool = getCuratedLgtmPool().filter((url) => url !== excludedUrl);
-  const targetPool = pool.length > 0 ? pool : getCuratedLgtmPool();
-
-  if (targetPool.length === 0) {
-    return null;
+  function getCuratedLgtmPool() {
+    return curatedLgtmGifs.slice();
   }
 
-  const index = Math.floor(randomFn() * targetPool.length);
-  return targetPool[index];
-}
+  function pickRandomLgtmGif(excludedUrl = null, randomFn = Math.random) {
+    const pool = getCuratedLgtmPool().filter((url) => url !== excludedUrl);
+    const targetPool = pool.length > 0 ? pool : getCuratedLgtmPool();
 
-lgtmRoot.GitHubMentionsLGTM = {
-  CURATED_LGTM_GIFS,
-  getCuratedLgtmPool,
-  pickRandomLgtmGif
-};
+    if (targetPool.length === 0) {
+      return null;
+    }
 
-if (typeof module !== 'undefined' && module.exports) {
-  module.exports = lgtmRoot.GitHubMentionsLGTM;
-}
+    const index = Math.floor(randomFn() * targetPool.length);
+    return targetPool[index];
+  }
+
+  root.GitHubMentionsLGTM = {
+    CURATED_LGTM_GIFS: curatedLgtmGifs.slice(),
+    getCuratedLgtmPool,
+    pickRandomLgtmGif
+  };
+
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = root.GitHubMentionsLGTM;
+  }
+})(typeof globalThis !== 'undefined' ? globalThis : this);
